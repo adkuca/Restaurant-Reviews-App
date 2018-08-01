2c929b71bba1e17d236a2b708a4e9465812d0a1a
Adam Kuča <ak.maggot@gmail.com>
2018-08-01 17:52:11 +0200

    sw update cache

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-var staticCacheName = 'page-static-v2';
+var staticCacheName = 'page-static-v1';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
+		'/',
 		'img/1.jpg',
 		'img/2.jpg',
 		'img/3.jpg',
@@ -19,10 +20,30 @@ self.addEventListener('install', function(event) {
 		'css/styles.css',
 		'js/main.js',
 		'js/restaurant_info.js',
+		'js/dbhelper.js',
+		'data/restaurants.json',
+		'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+		'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+		
       ]);
     })
   );
 });
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+	  caches.keys().then(function(cacheNames) {
+		return Promise.all(
+		  cacheNames.filter(function(cacheName) {
+			return cacheName.startsWith('page-') &&
+				   cacheName != staticCacheName;
+		  }).map(function(cacheName) {
+			return caches.delete(cacheName);
+		  })
+		);
+	  })
+	);
+  });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
